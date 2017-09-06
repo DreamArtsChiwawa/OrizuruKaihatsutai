f11eb8a8dd2b2ef49c619d13ca335c9aac89af53
Ryo Hanafusa <zwgntz@gmail.com>
2017-09-06 04:37:28 +0000

    search.pyの不具合修正

--- AI/search.py
+++ AI/search.py
@@ -6,14 +6,11 @@ import train
 from gensim import models
 from gensim.models.doc2vec import LabeledSentence
 
-INPUT_PATH = '../data/test_doc.txt'
+MODEL_PATH = './doc2vec.model'
 
-model = models.Doc2Vec.load('doc2vec.model')
-
-# 似た文章を探す
-def search_similar_texts(words):
+def _search_similar_texts(model, words, topn=10):
     x = model.infer_vector(words)
-    most_similar_texts = model.docvecs.most_similar([x])
+    most_similar_texts = model.docvecs.most_similar([x], topn=topn)
     sim_list = []
     for similar_text in most_similar_texts:
         tmp_sim_list = similar_text[0].split(",")
@@ -22,32 +19,26 @@ def search_similar_texts(words):
         sim_list.append(tmp_sim_list)
     return sim_list
 
-# 似た単語を探す
-def search_similar_words(words):
-    for word in words:
-        print()
-        print(word + ':')
-        for result in model.most_similar(positive=word, topn=10):
-            print(result[0])
-
-def search_similar_docs(wr_text):
+# 似た文章を探す
+def search_similar_docs(wr_text, topn=10):
     '''
     (ドキュメントのパス, 部署名前, 類似度)のタプルのリストを返す
     '''
     w, n = train.split_into_words(search_str)
     words = LabeledSentence(words=w, tags=[n]).words
-    sim_list = search_similar_texts(words)
+    model = models.Doc2Vec.load(MODEL_PATH)
+    sim_list = _search_similar_texts(model, words, topn=topn)
     return sim_list
     
 if __name__ == '__main__':
-    print("test document: " + INPUT_PATH)
-    search_str = train.read_document(INPUT_PATH)
-    print(train.trim_doc(search_str))
-    print("================\n\n")
-    for path, name, sim in search_similar_docs(search_str):
-        print(path)
-        
-        text, _ = train.trim_doc(train.read_document(path))
-        print(text)
-        print("-------------\n\n")
-    #print(search_similar_docs(search_str))
+    pass
+#    print("test document: " + INPUT_PATH)
+#    search_str = train.read_document(INPUT_PATH)
+#    print(train.trim_doc(search_str))
+#    print("================\n\n")
+#    for path, name, sim in search_similar_docs(search_str):
+#        print(path)
+#        
+#        text, _ = train.trim_doc(train.read_document(path))
+#        print(text)
+#        print("-------------\n\n")
