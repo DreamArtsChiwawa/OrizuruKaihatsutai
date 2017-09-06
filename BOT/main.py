680fd0927592e6805b081ba9aa1c9068ca73a9d1
Naoya Sugita <g1531072@m.sojo-u.ac.jp>
2017-09-06 18:02:59 +0900

    view wr_data

--- BOT/main.py
+++ BOT/main.py
@@ -48,6 +48,14 @@ def send_message(companyId, groupId, message):
     messageText = msgObj['text']
     ans = search.search_similar_docs(messageText,3)
     url = 'https://{0}.chiwawa.one/api/public/v1/groups/{1}/messages'.format(companyId, groupId)
+
+    wr = []
+    for i in range(len(ans)):
+        wr_path = ans[i][0]
+        wr.append(read_wr(wr_path))
+
+    print(wr)
+
     headers = {
         'Content-Type': 'application/json',
         'X-Chiwawa-API-Token': env['CHIWAWA_API_TOKEN']
@@ -66,13 +74,18 @@ def send_message(companyId, groupId, message):
                  'title': ans[1][1] + "さん",
                  'text':  ans[1][1] +"さんのwrのパスは" +  ans[1][0] + "にあります。"
          },{
-                 'attachmentId': 'slct2',
+                 'attachmentId': 'slct3',
                  'viewType': 'text',
                  'title': ans[2][1] + "さん",
                  'text':  ans[2][1] +"さんのwrのパスは" +  ans[2][0] + "にあります。"
          }],
     }
     requests.post(url, headers=headers, data=json.dumps(content))
+
+def read_wr(path):
+    with open(path) as f:
+        print(f.read())
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
