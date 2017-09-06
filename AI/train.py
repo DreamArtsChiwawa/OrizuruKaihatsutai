7e4fe0b498ed16741e440f10247897bdb0d3243d
Ryo Hanafusa <zwgntz@gmail.com>
2017-09-06 07:14:01 +0000

    train.py修正

--- AI/train.py
+++ AI/train.py
@@ -28,21 +28,22 @@ def read_document(path):
 
 # 青空文庫ファイルから作品部分のみ抜き出す
 def trim_doc(doc):
-    extracted_text = doc.replace("\n","").replace("-","").replace("—","") \
-            .replace("=","").replace("■","---").replace("◎", "")
-
-    m = re.search(r"Request(.*)---Highlight", extracted_text)
-    if m:
-        tmp_text = m.group(1)
+    extracted_text = re.sub(r'(-|=)', '', doc)
+    m1 = re.search(r"Request([\s\S]*?)Highlight", extracted_text)
+    if m1:    
+        tmp_text = m1.group(1)
     else:
         tmp_text = ''
 
-    m2 = re.search(r"Subject:(.*)(Message|Cc)", extracted_text)
-    if m2:
-        tmp_name = m2.group(1)
 
-    tmp_name = re.sub(r'\s', "", tmp_name)
+    m2 = re.search(r"Subject:([\s\S]*?)(Message|Cc)", extracted_text)     
+    tmp_name = m2.group(1)
+    m3 = re.search(r"\d{4}.?\d{,2}.?\d{,2}.?(.*)-?", tmp_name)  
+    if m3:
+        tmp_name = m3.group(1)
 
+    tmp_name = re.sub(r'\s', "", tmp_name)     
+    
     return (tmp_text, tmp_name)
 
 
