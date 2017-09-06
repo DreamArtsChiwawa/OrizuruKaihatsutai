8bb93535063ae2a281dd146346003ab87e5c45ff
Naoya Sugita <g1531072@m.sojo-u.ac.jp>
2017-09-06 09:51:24 +0900

    delet helloPythonBot

---
+++ BOT/parce_rec.py
@@ -0,0 +1,14 @@
+#def get_text():
+#    text = [("./to/dir/a.mes.utf","A部門 すぎた",0.8111),("./to/dir/a.mes.utf","A部門 すずき",0.5111),("./to/dir/a.mes.utf","B部門 やまだ",0.38),("./to/dir/a.mes.utf","C部門 たろう",0.01),("./to/dir/a.mes.utf","B部門 やまだ",0.18),("./to/dir/a.mes.utf","C部門 たろう",0.01),("./to/dir/a.mes.utf","B部門 やまだ",0.18),("./to/dir/a.mes.utf","C部門 たろう",0.01)]
+    return text
+
+#AI側からのレコメンド結果を受け取る
+def return_top3(message):
+    text = message
+    rec = []
+    for i in text:
+        if len(rec) != 3 and i not in rec:
+            rec.append(i)
+        else:
+            break
+    return rec
