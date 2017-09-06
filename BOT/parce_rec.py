a189a2d7d49de4832ce2be3916c26950fa4945f7
Naoya Sugita <g1531072@m.sojo-u.ac.jp>
2017-09-06 10:13:43 +0900

    add get_data

--- BOT/parce_rec.py
+++ BOT/parce_rec.py
@@ -1,3 +1,4 @@
+from ../AI/search.py import search_similar_docs
 #def get_text():
 #    text = [("./to/dir/a.mes.utf","A部門 すぎた",0.8111),("./to/dir/a.mes.utf","A部門 すずき",0.5111),("./to/dir/a.mes.utf","B部門 やまだ",0.38),("./to/dir/a.mes.utf","C部門 たろう",0.01),("./to/dir/a.mes.utf","B部門 やまだ",0.18),("./to/dir/a.mes.utf","C部門 たろう",0.01),("./to/dir/a.mes.utf","B部門 やまだ",0.18),("./to/dir/a.mes.utf","C部門 たろう",0.01)]
 #    return text
@@ -12,3 +13,7 @@ def return_top3(message):
         else:
             break
     return rec
+
+#sim_list = search_similar_docs()
+#rec = return_top3(sim_list)
+#print(rec)
