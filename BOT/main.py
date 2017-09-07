a35d6cc6a0bd19883eefc34a667e3187e3d32d3b
Naoya Sugita <g1531072@m.sojo-u.ac.jp>
2017-09-07 10:08:18 +0900

    add tell_me()

--- BOT/main.py
+++ BOT/main.py
@@ -26,10 +26,10 @@ def messages():
             messageText = msgObj['text']
             userName = msgObj['createdUserName']
             ans = search.search_similar_docs(messageText,3)
-            print("==========================")
-            print(ans)
             sndMsgText = '1. ' + ans[0][1] + '\n2. ' + ans[1][1] +  '\n3. ' + ans[2][1]
             send_message(companyId, groupId, userName + 'さん、週報を書いてくれてありがとう！あなたが抱えている課題は以前、この人も抱えていたみたいだから聞いてみると解決するかもしれないよ。\n' + sndMsgText)
+            if "おしえて" in message or "教えて" in message:
+                tell_me(companyId, groupId, userName)
 
             return "OK"
         else:
@@ -53,8 +53,6 @@ def send_message(companyId, groupId, message):
     for i in range(len(ans)):
         wr_path = ans[i][0]
         wr.append(read_wr(wr_path))
-
-    print(wr)
 
     headers = {
         'Content-Type': 'application/json',
@@ -87,6 +85,39 @@ def read_wr(path):
     with open(path) as f:
         wr_text += f.read()
     return wr_text
+
+def tell_me(companyId, groupId, message):
+    body = request.get_json(silent=True)
+    msgObj = body['message']
+    messageText = msgObj['text']
+    url = 'https://{0}.chiwawa.one/api/public/v1/groups/{1}/messages'.format(companyId, groupId)
+
+    headers = {
+        'Content-Type': 'application/json',
+        'X-Chiwawa-API-Token': env['CHIWAWA_API_TOKEN']
+    }
+    # content = {
+    #     'text' : message,
+    #     'attachments': [
+    #      {
+    #              'attachmentId': 'slct1',
+    #              'viewType': 'text',
+    #              'title': ans[0][1] + "さん",
+    #              'text': wr[0]
+    #     },{
+    #              'attachmentId': 'slct2',
+    #              'viewType': 'text',
+    #              'title': ans[1][1] + "さん",
+    #              'text':  wr[1]
+    #      },{
+    #              'attachmentId': 'slct3',
+    #              'viewType': 'text',
+    #              'title': ans[2][1] + "さん",
+    #              'text':  wr[2]
+    #      }],
+    # }
+    requests.post(url, headers=headers)
+    return ok
 
 
 if __name__ == '__main__':
